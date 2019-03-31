88851f2e3ba9ffeed7a81b822f4f96447e86c382
Kamry Bowman <kamry.bowman@gmail.com>
2019-03-30 18:17:59 -0600

    Implement WordCount function

--- src/com/lambdaschool/Main.java
+++ src/com/lambdaschool/Main.java
@@ -1,8 +1,37 @@
 package com.lambdaschool;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.function.Function;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("firetest");
+
+        File filePath = new File("text.txt");
+
+        HashMap<String, Integer> counter = new HashMap<>(50);
+
+        Scanner sc;
+        try {
+            sc = new Scanner(filePath);
+            while (sc.hasNext()) {
+                String word = sc.next();
+                counter.put(word, counter.getOrDefault(word, 0) + 1);
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<HashMap.Entry<String, Integer>> list = new ArrayList<>(counter.entrySet());
+        list.sort(Comparator.comparing((Function<Map.Entry<String, Integer>, Integer>) Map.Entry::getValue).reversed());
+        List<HashMap.Entry<String, Integer>> top50 = list.subList(0, 50);
+
+        for (HashMap.Entry<String, Integer> entry : top50) {
+            System.out.println("Word: " + entry.getKey() + ", Count: " + entry.getValue() + "\n");
+        }
     }
 }
